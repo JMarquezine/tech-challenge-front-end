16fd4261b0a56411fceab126060e9b0e2ef8e688
Juan Gustavo Marquezine Trindade <juanmarquezine@gmail.com>
2020-10-17 18:28:47 -0300

    Initialize and Setup Project

---
+++ src/bootstrap/App.tsx
@@ -0,0 +1,27 @@
+import React, { Suspense, ReactElement } from 'react'
+import {
+	CssBaseline,
+	StylesProvider,
+	createGenerateClassName,
+} from '@material-ui/core'
+import AppRouter from './AppRouter'
+
+class App extends React.Component {
+	public render(): ReactElement {
+		const generateClassName = createGenerateClassName({
+			productionPrefix: 'bexs-front-end-exam',
+			seed: 'bexs-front-end-exam',
+		})
+
+		return (
+			<Suspense fallback={null}>
+					<CssBaseline />
+					<StylesProvider generateClassName={generateClassName}>
+						<AppRouter />
+					</StylesProvider>
+			</Suspense>
+		)
+	}
+}
+
+export default App
---
+++ src/bootstrap/AppRouter.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
+
+const AppRouter: React.FC = () => {
+	return (
+		<Router>
+			<Switch>
+				<Route path={`/`} >
+                    <div>Test</div>
+                </Route>
+			</Switch>
+		</Router>
+	)
+}
+
+export default AppRouter
